5b7f9b486128ea755b483385b8735a0b34608608
Dru <91165792+awals829@users.noreply.github.com>
2025-02-04 18:52:46 -0700

    pushity push push

--- data_parser.py
+++ data_parser.py
@@ -4,14 +4,35 @@ import mpl_toolkits as mpl
 
 filename = "random_coordinates_energy.csv"
 
+node_nums = []
+x_coords = []
+y_coords = []
+z_coords = []
+energy = []
+
 with open(filename, mode='r', newline='') as file:
     reader = csv.DictReader(file)
 
     for row in reader:
-        node_number = int(row['Node Number'])
-        x = float(row['X'])
-        y = float(row['Y'])
-        z = float(row['Z'])
-        energy = int(row['Energy'])
+        node_nums.append(int(row['Node Number']))
+        x_coords.append(float(row['X']))
+        y_coords.append(float(row['Y']))
+        z_coords.append(float(row['Z']))
+        energy.append(int(row['Energy']))
 
-        print(f"Node Number: {node_number}, x:{x}, y: {y}, z: {z}, energy: {energy}")
+
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+
+marker_sizes = [10 * e for e in energy]
+
+ax.scatter(x_coords, y_coords, z_coords, c='purple', marker='x', s=marker_sizes)
+
+# Label each node with its energy value
+for i in range(len(x_coords)):
+    # Adjust fontsize and color as needed
+    ax.text(x_coords[i], y_coords[i], z_coords[i], f"{node_nums[i]}", 
+            fontsize=12, color='black')
+
+ax.plot(x_coords, y_coords, z_coords, marker='o')
+plt.show()
