51acae37d8496507cc1b697e4a87f0ae19b6bd23
ewush956 <ewush956@mtroyal.ca>
2025-02-09 16:03:55 -0700

    debug

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def solve(graph : Graph, node : FoodItem) -> None:
     
     if(len(graph.remaining_food) == 1):
         graph.current_path.path_list.append(node)
+        print("updating optimal")
+        print(graph.current_path)
         graph.update_optimal()
         return
     
@@ -16,6 +18,7 @@ def solve(graph : Graph, node : FoodItem) -> None:
 
     for food in graph.remaining_food:
         print(graph.current_path)
+        
         #curr_food = graph.remaining_food[food.food_id]
         #curr_food = graph.remaining_food[food[0]]
         #next_food = graph.remaining_food[food.food_id + 1]
@@ -26,13 +29,13 @@ def solve(graph : Graph, node : FoodItem) -> None:
         cost = get_total_cost(current_node, food)
         if (cost <= curr_path.net_energy_gain):
             net = food.energy - cost
-            curr_path.update_net(net)
+            curr_path.update_net(-cost)
             solve(graph, food)
-            curr_path.update_net(-net)
-            graph.remaining_food.insert(current_node.food_id, current_node)
-            graph.current_path.path_list.remove(current_node)
-    #graph.remaining_food.insert(current_node.food_id, current_node)
-    #graph.current_path.path_list.remove(current_node)
+            curr_path.update_net(cost)
+            #graph.remaining_food.insert(current_node.food_id, current_node)
+            #graph.current_path.path_list.remove(current_node)
+    graph.remaining_food.insert(current_node.food_id, current_node)
+    graph.current_path.path_list.remove(current_node)
     return
     """
     if p is solved:
@@ -53,6 +56,7 @@ def main():
     graph.current_path.net_energy_gain = graph.remaining_food[0].energy
     starting_node = graph.all_food_nodes[0]
     solve(graph, starting_node)
+    print("Optimal:")
     print(graph.optimal_path)
 
 if __name__ == "__main__":
