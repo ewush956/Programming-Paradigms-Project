c6da4f99fe04ac152e8010a29bb9e21449dd8db8
ewush956 <ewush956@mtroyal.ca>
2025-02-10 13:22:01 -0700

    cleanup

--- main.py
+++ main.py
@@ -5,38 +5,32 @@ from calculation_tester import get_total_cost
 import bisect
 
 def solve(graph : Graph, node : FoodItem) -> None:
-    #print(graph.current_path)
-    #print(graph.remaining_food)
-    #print()
+
+    print(f"Current Path: {graph.current_path}")
+    print(f"Net Gain: {graph.current_path.net_energy_gain}")
+    print()
     if(len(graph.remaining_food) == 0):
-        graph.update_optimal()
         print(f"Current Optimal: {graph.optimal_path}")
         print(f"Net Gain: {graph.optimal_path.net_energy_gain}")
+        print()
+        graph.update_optimal()
+
         return
-    #graph.remaining_food.remove(node.food_id)
-    # graph.current_path.path_list.append(node)
+
     for food in graph.remaining_food:
-        #print(graph.current_path)
-        #print("AAA")
-        #print(graph.current_path.all_food_nodes[food])
-        #graph.remaining_food.remove(node.food_id)
-        #graph.current_path.path_list.append(node)
-        # print(graph.remaining_food)
         cost = get_total_cost(node, graph.all_food_nodes[food])
+
         if(graph.current_path.net_energy_gain >= cost):
             graph.remaining_food.remove(food)
             graph.current_path.path_list.append(food)
-            #graph.current_path.path_list.append(node.food_id)
             graph.current_path.net_energy_gain -= cost
+
             solve(graph, graph.all_food_nodes[food])
+
             graph.current_path.net_energy_gain += cost
-            #graph.current_path.path_list.remove(graph.all_food_nodes[food])
-            #graph.current_path.path_list.remove(graph.all_food_nodes[food].food_id)
             graph.current_path.path_list.remove(food)
-            #graph.remaining_food.append(food)
             bisect.insort(graph.remaining_food, food)
         
-    
 def main():
     graph = Graph()
     graph.read_csv_data("./random_coordinates_energy.csv")
@@ -46,8 +40,8 @@ def main():
     graph.remaining_food.remove(0)
     graph.current_path.path_list.append(0)
     solve(graph, starting_node)
-    print("optimal:")
-    print(graph.optimal_path)
+    print(f"optimal: {graph.optimal_path}")
+    print(f"net: {graph.optimal_path.net_energy_gain}")
 
 if __name__ == "__main__":
     main()
