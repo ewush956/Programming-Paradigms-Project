3f00413f6958656858d0abb72c2f341b57ef8655
Dru <91165792+awals829@users.noreply.github.com>
2025-02-20 15:12:48 -0700

    removed csv from food item.

--- food_item.py
+++ food_item.py
@@ -7,9 +7,6 @@ class FoodItem():
         self.y = y
         self.z = z
         self.energy = energy
-        
+    
     def __str__(self):
         return (f"Food Id:{self.food_id}\nX-Coord:{self.x}\nY-Coord:{self.y}\nZ-Coord:{self.z}\nEnergy:{self.energy}\n")
-    
-    def format_csv(self) -> str:
-        return (f"{self.food_id},{self.x},{self.y},{self.z},{self.energy}")
